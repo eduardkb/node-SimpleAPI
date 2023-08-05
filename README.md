d925c237c5ef6aaace1b33c3d8b3d7c84e73037f
eduardbuhali <eduard.k.buhali@exxonmobil.com>
2023-08-05 12:10:00 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description
+    - Very simple API in Node.js
+
+# initialize Node.js Project
+    - npm init
+    - npm install express
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+const items = ['apple', 'banana', 'orange'];
+app.get('/items', (req, res) => {
+    res.json(items);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
