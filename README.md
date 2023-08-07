30dee65612e154ad21abe4fae10e0bd98e9f8234
eduardkb <eduardbuhali@gmail.com>
2023-08-07 11:42:57 -0300

    testing AZ ci/cd deployment 3

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Description
     - Very simple API in Node.js
+    - web.config file: needed to deploy node to web app in Azure
+
+# API's
+    - /             --> simple web page
+    - /items        --> returns a json array
 
 # Run project
     - node .\server.js
--- server.js
+++ server.js
@@ -1,40 +1,29 @@
 const express = require('express');
 const app = express();
 
-const sName = process.env.NAME || 'default';
-console.log('>>> DEBUG_NAME_VAR:', sName);
-
+const sName = process.env.NAME || 'NO_NAME_VALUE';
+const sConnStr = process.env.CONN || 'NO_CONN_VALUE';
 app.get('/', (req, res) => {
-    if (sName === 'default') {
-        res.send('Hello. Page is working!');
-    }
-    else {
-        res.send(`Hello.\nVar content:, ${sName}`)
-    }
-});
-
-
-
-app.get('/page', (req, res) => {
     var sHtml = `
 <html>
 
 <head>
-  <title>Debug page</title>
+  <title>EKB Welcome Title</title>
 </head>
 
-<body style="background-color: #0099ff;">
-  <h3>Debug information</h3>
+<body style="background-color: #99dbe9;">
+  <h3>EKB Welcome Page</h3>
+  <BR/>
+  <BR/>
+  <strong>Variables:</strong>
   <ul>
-    <li><strong>Request Url</strong><span>: Test URL</span></li>
-    @* <li><strong>Outbound public IP</strong><span>: Test PUBIP</span></li> *@
-    <li><strong>Inbound client IP</strong><span>: Test serv var</span></li>
+    <li><strong>NAME Var Content</strong><span>: ${sName}</span></li>
+    <li><strong>CONN Var Content</strong><span>: ${sConnStr}</span></li>
   </ul>
-  <strong>Headers</strong>
+  <strong>API's</strong>
   <ul>
-      <li><strong>Item 1</strong><span>: Test 1</span></li>
-      <li><strong>Item 2</strong><span>: Test 2</span></li>
-      <li><strong>Item 3</strong><span>: Test 3</span></li>    
+      <li><strong>API 1</strong><span>: /items</span></li>
+      <li><strong>API 2</strong><span>: <under development></span></li>      
   </ul>
 </body>
 
@@ -45,12 +34,15 @@ app.get('/page', (req, res) => {
 
 const items = ['Copo', 'Mouse', 'Relogio', 'Fone'];
 app.get('/items', (req, res) => {
-    res.json(items);
+    const aRet = {
+        status: "Success",
+        code: "200",
+        result: items
+    }
+    res.json(aRet);
 });
 
 const port = process.env.PORT || 3000;
-console.log('>>> DEBUG_PORT_VAR:', port);
-
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
