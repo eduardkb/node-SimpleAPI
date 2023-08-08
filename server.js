d8e45511cdc8890e5ee4b6758a60f5e5066d0d1d
eduardkb <eduardbuhali@gmail.com>
2023-08-08 08:20:05 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const app = express();
 const sName = process.env.NAME || 'NO_NAME_VALUE';
 const sConnStr = process.env.CONN || 'NO_CONN_VALUE';
 app.get('/', (req, res) => {
-    var sHtml = `
+  var sHtml = `
 <html>
 
 <head>
   <title>EKB Webpage</title>
 </head>
 
-<body style="background-color: #cc6600;">
+<body style="background-color: #a3c2c2;">
   <h3>EKB Welcome</h3>
   <BR/>
   <BR/>
@@ -29,20 +29,20 @@ app.get('/', (req, res) => {
 
 </html>
 `
-    res.send(sHtml);
+  res.send(sHtml);
 });
 
 const items = ['Lamp', 'Heater', 'Humidifyer', 'Courtain'];
 app.get('/items', (req, res) => {
-    const aRet = {
-        status: "Success",
-        code: "200",
-        result: items
-    }
-    res.json(aRet);
+  const aRet = {
+    status: "Success",
+    code: "200",
+    result: items
+  }
+  res.json(aRet);
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
